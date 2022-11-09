6429904949cd3d6d7117e3cbcb27d44762f43ba0
rajesh vaduguru <109016951+Rajcode123@users.noreply.github.com>
2022-11-09 19:54:22 +0530

    updated file uploaded

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ for(var i=0; i < numberOfdrums ; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
         var buttonInnerHTML = this.innerHTML ;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
      
     });
 
 document.addEventListener("keydown" , function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -45,5 +47,14 @@ function makeSound(key){
             
             break;
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout (function(){
+        activeButton.classList.remove("pressed");
+    },100);
 }
 }
